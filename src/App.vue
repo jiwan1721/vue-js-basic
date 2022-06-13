79416582e9d089a4a446bef59c80c71864c53020
jiwan chanda <chandjiwn9@gmail.com>
2022-06-13 17:42:06 +0545

    veujs basics learned about components

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="app">
+  <!-- <Todos id="1" title="basic" completed="true"/> -->
+  <h1>To-do-list</h1>
+  <ul>
+    <li>
+      <Done-First label="are you ready"></Done-First>
+    </li>
+  </ul>
+
+</div>
+
+</template>
+
+<script>
+import DoneFirst from './components/DoneFirst.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      DoneFirst
+    },
+  
+  };
+</script>
+
+<style scoped>
+  /* *{
+    box-sizing: border-box;
+    margin:0;
+    padding:0;
+  }
+  body {
+    font-family: Arial,Helvetica,sans-serif;
+    line-height:1.4;
+  } */
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Todos {{id}} :{{title}}: {{completed}}</h1>
+        <input type="checkbox" :value="true">
+    </div>
+</template>
+<script>
+
+export default {
+    name:"todoItem",
+    props: {
+        id: {
+            type: Number,
+        },
+        title:{
+            type: String,
+        },
+        completed: {
+            type: Boolean,
+            default: true
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
