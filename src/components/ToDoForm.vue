f905e2f6a08ff801a510f9f77ecb2a358f5fbbb2
jiwan chanda <chandjiwn9@gmail.com>
2022-06-14 18:13:53 +0545

    css vues

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <form @submit.prevent="onSubmit"> <!-- yesley page reload huna didaiina -->
+        <label for="new-todo-input">
+            what needs to be done?
+        </label>
+        <input
+            type="text"
+            id="new-todo-input"
+            name="new-todo"
+            autocomplete="off"
+            v-model.lazy.trim="label"
+            class="input__lg"
+        />
+        <button type="submit" class="btn btn__primary btn__lg">Add</button>
+    </form>
+</template>
+<script>
+    export default{
+        methods: {
+            onSubmit(){
+                console.log('label value: ',this.label);
+                if(this.label=== ""){
+                    return;
+                }
+                this.$emit("todo-added",this.label);
+                this.label="";
+            }
+        },
+        //we need a way to get the value from the forms input
+        data() {
+            return {
+                label: ""
+            };
+        }
+    };
+</script>
